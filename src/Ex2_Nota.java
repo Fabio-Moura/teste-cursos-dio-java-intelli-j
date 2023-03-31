77f63fcc0d0eee9218f06e587226f659a0cbed42
frm <fabio.moura@outlook.com>
2023-03-30 23:17:40 -0300

    Primeiro commit

---
+++ src/Ex2_Nota.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Ex2_Nota {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner (System.in);
+
+        int nota;
+
+        System.out.println("Nota: ");
+        nota = scan.nextInt();
+    }
+
+
+}
--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -8,7 +8,7 @@ public class PrimeiroPrograma {
         Gato gato = new Gato ();
         System.out.println(gato);
         int b = 3;
-        System.out.println("Hello World " + (a+b));*/
+//        System.out.println("Hello World " + (a+b));
     }
 
 }
